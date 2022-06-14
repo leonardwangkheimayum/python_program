bbef049f459fd539a71a55373230c69ce75470e4
leonard <leonardwangkheimayum@gmail.com>
2022-06-14 07:47:37 +0530

    class_1(input)

--- class_1.py
+++ class_1.py
@@ -1,4 +1,6 @@
-print("Leonard")
-a=10
-print("a value is ",a)
-print("a value is {0}".format(a))
+# print("Leonard")
+# a=10
+# print("a value is ",a)
+# print("a value is {0}".format(a))
+a=input("Enter the Number")
+print("Enter value",a)
