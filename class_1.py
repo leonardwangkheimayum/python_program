2373f3373c15cf822d73ad5bf5896dc1e4afc01d
leonard <leonardwangkheimayum@gmail.com>
2022-06-15 07:41:53 +0530

    class_2 operators

--- class_1.py
+++ class_1.py
@@ -2,5 +2,114 @@
 # a=10
 # print("a value is ",a)
 # print("a value is {0}".format(a))
-a=input("Enter the Number")
-print("Enter value",a)
+# a=input("Enter the Number")
+# print("Enter value",a)
+#
+# keywords
+# varaibles
+# operators
+# debug
+# basic data type ->int,float,string,chr,bool
+#
+# keywords --> int,float,string,chr,bool
+#
+# variables->temp storage store data
+
+
+#a=12
+# aa=23
+# name=12
+# name123=2345
+#aQ12
+#a_1
+
+
+# a b=1
+# a#=1
+# a$=7
+# 1ast
+
+# debug--> developer options
+# break point red line
+# f8 f7
+
+# a=12
+# b=23
+# print(a)
+
+# int --numbers
+# float->decimal values
+# str-"python programming "
+# chr-'' or "a"
+# bool true or false
+
+
+# operators
+# arthmetic operator ->+,-,*,/(quotient),%(remainder)
+# print(11/2)
+# print(11//2)
+
+# relational operator -> <,>=,>,<=,==,!=
+# a=1
+# b=3
+# print(a < b)
+
+# logical
+# # and,or,not
+# T T T
+# T F F
+# F F F
+# F T  F
+
+
+# T T T
+# T F T
+# F F F
+# F T T
+#
+#
+# T F
+# F T
+#
+# and or not
+#
+# binary
+# & | !
+
+# 0 0 0
+# 1 1 1
+# 0 1 0
+# 1 0 0
+#
+#
+# 0 0 0
+# 1 0 1
+# 0 1 1
+# 1 1 1
+#
+# 1 0
+# 0 1
+
+# Identity and member ship
+# in/ not  in
+# is is not
+
+
+# print("j" in "java")
+# print("j" not in "java")
+
+
+# print("java" is "java")
+# print("java" is not  "java")
+
+
+#assignment operator
+=
+
+# a=1
+# b=2
+# c=4
+#
+#
+# a=a+1 -> a+=1
+# a=a+12 -> a+=12
