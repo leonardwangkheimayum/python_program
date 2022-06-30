f04b1bc8aeaf2d0221d4cc1ce85da185a8c2a763
leonard <leonardwangkheimayum@gmail.com>
2022-06-30 08:00:24 +0530

    class 7(funcion)

---
+++ funques.py
@@ -0,0 +1,34 @@
+#with paramter with return
+def sum( x, y ):
+  a=x+y
+  return a
+if __name__ == "__main__":#header of code/statement
+    result=sum(67,1)
+    print(result)
+
+    # with paramter no return
+def sum(x,y):
+    sum=x+y
+    print(sum)
+if __name__ == "__main__":  # header of code/statement
+    sum(15,1)
+#no paramater no return
+def sum():
+    x=2
+    y=3
+    a=x+y
+    print(a)
+if __name__ == "__main__":  # header of code/statement
+    sum()
+#no parameter with return
+def div():
+    a=8
+    b=2
+    c=a//b
+    return c
+if __name__ == "__main__":  # header of code/statement
+    gg=div()
+    print(gg)
+
+
+
